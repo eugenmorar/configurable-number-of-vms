41a8091de1bf25bd8d0d0f0117d91434f8fba0c7
Eugen Morar <eugen.morar@arondmessaging.ro>
2024-07-04 10:30:48 +0300

    no purge

--- ping.sh
+++ ping.sh
@@ -6,7 +6,6 @@ CURRENT_PUBLIC_IP=$3
 CURRENT_PRIVATE_IP=$4
 NEXT_PRIVATE_IP=$5
 
-echo > results
 ssh -o StrictHostKeyChecking=no -i assessment-tf.pem ubuntu@${CURRENT_PUBLIC_IP} /bin/bash <<EOF
 if ping -c 1 ${NEXT_PRIVATE_IP} &> /dev/null; then
     echo "ping from VM[${CURRENT_INDEX}] - ${CURRENT_PRIVATE_IP} to VM[${NEXT_INDEX}] - ${NEXT_PRIVATE_IP} is successfull" 
